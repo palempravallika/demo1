336039e3e5d1f6a8959c85c89d99f518538b9e18
palempravallika <palempravallika09@gmail.com>
2025-04-08 12:45:13 +0530

    changed the file

--- cafe/getAppParameters.php
+++ cafe/getAppParameters.php
@@ -49,31 +49,31 @@ $db_password = "";
 
 try {
   $db_url = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/dbUrl'
+    'SecretId' => '/cafe/dbUrl1'
   ]);
   $db_url = $db_url["SecretString"];
   $db_user = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/dbUser'
+    'SecretId' => '/cafe/dbUser1'
   ]);
   $db_user = $db_user["SecretString"];
   $db_password = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/dbPassword'
+    'SecretId' => '/cafe/dbPassword1'
   ]);
   $db_password = $db_password["SecretString"];
   $db_name = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/dbName'
+    'SecretId' => '/cafe/dbName1'
   ]);
   $db_name = $db_name["SecretString"];
   $currency = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/currency'
+    'SecretId' => '/cafe/currency1'
   ]);
   $currency = $currency["SecretString"];  
   $timezone = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/timeZone'
+    'SecretId' => '/cafe/timeZone1'
   ]);
   $timezone = $timezone["SecretString"];  
   $showServerInfo = $secrets_client->getSecretValue([
-    'SecretId' => '/cafe/showServerInfo'
+    'SecretId' => '/cafe/showServerInfo1'
   ]);
   $showServerInfo = $showServerInfo["SecretString"];  
 
